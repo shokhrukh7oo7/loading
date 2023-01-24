35a607d3cf08162a12d83e186926faa3a3428f3c
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2023-01-25 01:19:06 +0500

    first commit

---
+++ index2.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", () => {
+    document.getElementById("loading").style.display = "none";
+  });
+  // or
+  setTimeout(() => {
+    document.getElementById("loading").style.display = "none";
+  }, 2000);
